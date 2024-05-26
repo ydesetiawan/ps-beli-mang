1c8769a798a41c6fcce4b132004e7e40380ea7a2
Edy Setiawan <edy.setiwan@mekari.com>
2024-05-26 13:49:58 +0700

    DB Migration for purchase

--- db/migrations/000002_create_table_merchants.up.sql
+++ db/migrations/000002_create_table_merchants.up.sql
@@ -3,18 +3,18 @@ CREATE TABLE merchants (
    name VARCHAR(30) NOT NULL,
    merchant_category  VARCHAR(30) NOT NULL,
    image_url VARCHAR(2048) NOT NULL,
-   location_lat FLOAT NOT NULL,
-   location_long FLOAT NOT NULL,
+   loc_lat FLOAT NOT NULL,
+   loc_long FLOAT NOT NULL,
    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE "merchant_item" (
-    "id" char(26) PRIMARY KEY,
-    "merchant_id" char(26) NOT NULL ,
-    "name" VARCHAR(30) NOT NULL ,
-    "category" VARCHAR(30),
-    "image_url" VARCHAR(2048),
-    "price" NUMERIC NOT NULL ,
-    "created_at" timestamp,
+CREATE TABLE merchant_items (
+    id char(26) PRIMARY KEY,
+    merchant_id char(26) NOT NULL ,
+    name VARCHAR(30) NOT NULL ,
+    category VARCHAR(30),
+    image_url VARCHAR(2048),
+    price NUMERIC NOT NULL ,
+    created_at timestamp,
     FOREIGN KEY (merchant_id) REFERENCES merchants(id)
 );
--- db/migrations/000003_create_table_orders.up.sql
+++ db/migrations/000003_create_table_orders.up.sql
@@ -1,1 +1,24 @@
+CREATE TABLE orders (
+    id char(26) PRIMARY KEY,
+    total_price NUMERIC NOT NULL,
+    delivery_time INT NOT NULL,
+    is_order BOOLEAN NOT NULL default false,
+    user_loc_lat FLOAT NOT NULL,
+    user_loc_long FLOAT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
 
+CREATE TABLE order_items (
+    id char(26) PRIMARY KEY,
+    order_id char(26) NOT NULL ,
+    merchant_id char(26) NOT NULL ,
+    is_starting_point BOOLEAN NOT NULL  default false,
+    merchant_item_id char(26) NOT NULL ,
+    quantity INT NOT NULL ,
+    price NUMERIC NOT NULL ,
+    amount NUMERIC NOT NULL ,
+    created_at timestamp,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (merchant_id) REFERENCES merchants(id),
+    FOREIGN KEY (merchant_item_id) REFERENCES merchant_items(id)
+);
