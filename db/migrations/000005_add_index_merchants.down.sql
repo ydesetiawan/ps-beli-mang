54c035f7719fb6e5ea5531bf1be298be541d9b3d
yusuf.gautama <yusuf.gautama@mekari.com>
2024-06-01 17:44:17 +0700

    update: remove index

--- db/migrations/000005_add_index_merchants.down.sql
+++
@@ -1,7 +0,0 @@
-DROP INDEX IF EXISTS merchants_idx_category;
-DROP INDEX IF EXISTS merchants_idx_name;
-DROP INDEX IF EXISTS merchants_idx_created_at;
-DROP INDEX IF EXISTS merchant_items_idx_merchant_id;
-DROP INDEX IF EXISTS merchant_items_idx_name;
-DROP INDEX IF EXISTS merchant_items_idx_category;
-DROP INDEX IF EXISTS merchant_items_idx_created_at;
